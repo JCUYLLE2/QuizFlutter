053894780efb62349a604c2c7d2e07522c871e28
JCUYLLE2 <162974022+JCUYLLE2@users.noreply.github.com>
2024-04-12 19:08:10 +0200

    aanpassen vragen

--- lib/settings.dart
+++ lib/settings.dart
@@ -15,81 +15,51 @@ List<dynamic> questions = [
     "imageHeight": 200.0, // Adjust the height of the image
   },
   {
-    "question": "Uit welke film komt deze foto?",
+    "question": "Vanwaar kennen we Mia Wallace?",
     "image": "images/imagepf.jpg",
     "answers": ["Amelie Poulin", "Thuis", "Jacky Brown", "Pulp Fiction"],
     "correct": 3
   },
   {
-    "question": "Uit welke film of komt deze gif?",
+    "question": "Wie viert hier zijn 11e verjaardag?",
     "image": "images/gifhp.gif",
-    "answers": [
-      "Home Alone",
-      "    Harry Potter &    de steen der wijzen",
-      "Home Alone 2",
-      "Aladin"
-    ],
+    "answers": ["Home Alone", "Harry Potter", "Home Alone 2", "Aladin"],
     "correct": 1
   },
   {
     "question": "Uit welke film komt deze foto?",
     "image": "images/imagestp.jpg",
-    "answers": ["The Lion King", "Mulan", "Star Wars", "De kleine zeemeermin"],
+    "answers": ["The Lion King", "Mulan", "Star Wars", "Assepoester"],
     "correct": 0
   },
   {
     "question": "Uit welke serie kennen we dit personage?",
     "image": "images/gifst2.gif",
-    "answers": [
-      "E.T.",
-      "Close Encounters of the Third Kind",
-      "The Goonies",
-      "Stranger Things"
-    ],
+    "answers": ["E.T.", "Sex Education", "The Goonies", "Stranger Things"],
     "correct": 3
   },
   {
     "question": "Uit welke reeks komt deze foto?",
     "image": "images/gifgot.gif",
-    "answers": [
-      "The Lord of the Rings",
-      "Game of Thrones",
-      "The Chronicles of Narnia",
-      "Harry Potter and the Goblet of Fire"
-    ],
+    "answers": ["LOTR", "Game of Thrones", "Narnia", "Harry Potter"],
     "correct": 1
   },
   {
     "question": "Uit welke reeks komt deze foto?",
     "image": "images/imagewd.jpg",
-    "answers": [
-      "Fear the Walking Dead",
-      "The Walking Dead",
-      "Zombieland",
-      "World War Z"
-    ],
+    "answers": ["Shameless", "The Walking Dead", "Zombieland", "World War Z"],
     "correct": 1
   },
   {
     "question": "Uit welke film komt deze foto?",
     "image": "images/giftm.gif",
-    "answers": [
-      "Back to the Future",
-      "Interstellar",
-      "Inception",
-      "The Matrix"
-    ],
+    "answers": ["BTTF", "Interstellar", "Inception", "The Matrix"],
     "correct": 3
   },
   {
     "question": "Uit welke film komt deze knapperd?",
     "image": "images/imagelotr.jpg",
-    "answers": [
-      "Jurassic Park",
-      "Indiana Jones",
-      "The Lord of the Rings",
-      "Jaws"
-    ],
+    "answers": ["Jurassic Park", "Indiana Jones", "LOTR", "Jaws"],
     "correct": 2
   },
   {
